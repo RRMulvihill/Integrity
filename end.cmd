21e75ddebe79cbb2b663bbfc8da8c406cbbf3254
Riley Mulvihill <Friskog55@GMail.com>
2018-06-05 10:06:52 -0400

    Update end.cmd

--- end.cmd
+++ end.cmd
@@ -1,3 +1,3 @@
 @echo off
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
-C:/Integrity_Test/TestGenius/Integrity/End_Script.py > C:/Integrity_Test/TestGenius/Integrity/logs/%mydate%.txt
+Path/To/End_Script.py > Path/To/Integrity/logs/%mydate%.txt
