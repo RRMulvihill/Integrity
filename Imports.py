9f8f325b900c872c59a69d6341872e2ecfa36188
Riley Mulvihill <Friskog55@GMail.com>
2018-06-05 10:05:21 -0400

    Delete Imports.py

--- Imports.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-filelist = (open("filelist.txt").readline())
-path = "C:/Integrity_Test/TestGenius/"
-integrity_path = path + "Integrity/"
-move_to = integrity_path + "removed_files/"
-today = str(datetime.date.today())
-date_path =move_to + today + "/"
-backup_destination = integrity_path + "backups/" +today
-ignore_folder = "backups"
