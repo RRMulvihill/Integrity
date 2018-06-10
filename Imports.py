01d5718248a83090bd96dc0de6013cb044e481f8
Riley Mulvihill <Friskog55@GMail.com>
2018-06-10 14:48:03 -0400

    Add files via upload

--- Imports.py
+++ Imports.py
@@ -1,9 +1,25 @@
-from datetime import datetime, date
-filelist = (open("filelist.txt").readline())
-path = '' #path/to/mainfolder
-integrity_path = path + "Integrity/"
+'''
+IMPORTS
+by Riley Mulvihill
+
+These are the hardcoded variables used by
+Start_Script.py and End_Script.py
+
+If the TestGenius directory is moved,
+THESE PATHS MUST BE UPDATED.
+'''
+
+from datetime import datetime,date
+import os
+
+
+path = "C:/TestGenius/"
+integrity_path = path + "/integrity/"
 move_to = integrity_path + "removed_files/"
-today = str(datetime.date.today())
+today = str(date.today())
 date_path =move_to + today + "/"
-backup_destination = integrity_path + "backups/" +today
+backup_destination = "C:/TestGenius_Backups/backups/" +today
+log_destination = "C:/TestGenius_Backups/logs/"
 ignore_folder = "backups"
+
+
