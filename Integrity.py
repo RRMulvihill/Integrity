e6816698ca52c4cb128534b8c0adbec31cd14b4e
Riley Mulvihill <Friskog55@GMail.com>
2018-07-13 10:52:18 -0400

    Add files via upload

--- Integrity.py
+++ Integrity.py
@@ -1,13 +1,27 @@
 '''
-END_SCRIPT
+Integrity
 by Riley Mulvihill
 
-End_Script is the main framework to the integrity program.
+A Data Assurance Program built for the OPAC testing software
 It primarily does two actions:
 1. Remove unwanted files
 2. Creates Backups
 
-End_Script should be run at least on a daily basis after Start_Script.py
+Leave Integrity running in the background.
+
+Integrity saves the initial state of its target folder
+at START_TIME
+
+For every time in RUN_TIMES, Itegrity will create a backup of all
+directories and sub-directories in the target folder
+
+Any files in the target folder NOT included in the initail state
+will be transfered to the REMOVED_PATH
+
+Once the BACKUP_FOLDER has more buckups stored than the
+BACKUPS_TO_KEEP variable, then Integrity will remove the oldest.
+
+Integrity will not run on weekends and will resume on Monday.
 
 '''
 
@@ -15,13 +29,34 @@ End_Script should be run at least on a daily basis after Start_Script.py
 #Import Modules
 import os
 import shutil
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 from time import sleep
 from Imports import *
 from subprocess import Popen
 
+if os.path.isfile('filelist.txt'):
+    filelist = open('filelist.txt')
+else:
+    filelist = open('filelist.txt','w')
+
+
 
 #Declare Functions
+
+def compare_logs(log1,log2):
+    change_log = []
+    for file in log1:
+        if file not in log2:
+            change_log.append(file)
+    return change_log
+
+def change_log(source):
+    files = []
+    for dirName, subdirList, fileList in os.walk(source):
+        #print('Found Directory: %s' % dirName)
+        for fname in fileList:
+            files.append(fname)
+    return( files)
 
 def compare_changes(original, new):
     '''
@@ -70,16 +105,18 @@ def clean_backups(source, quantity_to_keep):
         The folder names of deleted files
 
     '''
+    log_string = ''
     backups = []
     for backup in os.listdir(source):
         backups.append(backup)
     while (len(backups) > quantity_to_keep):
         if os.path.exists(source + backups[0]):
             shutil.rmtree(source + backups[0])
-            print (backups[0] + "removed")
+            log_string += (backups[0] + "removed\n")
             backups.pop(0)
         else:
-            print('no backup to remove')
+            log_string +=('no backup to remove\n')
+    return log_string
 
             
 def create_backups(source, destination, ignore_folder=None):
@@ -98,24 +135,27 @@ def create_backups(source, destination, ignore_folder=None):
     ignore_folder : string
         name of backup folder(to prevent looping)
 
-    Returns
+    Returns : String
     -------
     None
     '''
+    log_string = ''
     if os.path.exists(destination):
-        print("There is already a backup for " + destination)
+        log_string += ("There is already a backup for " + destination + '\n')
     else:
         os.makedirs(destination)
-        print('Backup Created at ' + destination)
+        log_string += ('Backup Created at ' + destination + '\n')
         copytree(source,destination,
                  ignore= shutil.ignore_patterns(ignore_folder))
+    return log_string
 
 def create_filelist(source):
     '''
-    Creates a list of files.
+    Creates a filelist text file.
 
-    Returns a list of all files in the 'source' folder
-    to be exported into filelist.txt by Start.cmd.
+    Creates a text file containing each
+    file name of the source directory
+    on a new line.
 
     Parameters
     ----------
@@ -124,14 +164,14 @@ def create_filelist(source):
 
     Returns
     -------
-    li
-        list of filenames as strings
+    None
     '''
     
-    filelist = []
+    filelist = open('filelist.txt','w')
     for file in os.listdir(source):
-        filelist.append(file)
-    print(filelist)
+        filelist.write(file + '\n')
+    filelist.close()
+    return
 
 
 def copytree(source, destination, symlinks=False, ignore=None):
@@ -154,8 +194,9 @@ def copytree(source, destination, symlinks=False, ignore=None):
 
     Returns
     -------
-        None
+        String
     '''
+    log_string = ''
     if not os.path.exists(destination):
         os.makedirs(destination)
     for item in os.listdir(source):
@@ -163,7 +204,7 @@ def copytree(source, destination, symlinks=False, ignore=None):
         if item == "backups":
             print("Loop stopped at backups")
             return
-        print(item)
+        log_string += (item + '\n')
         tree_source = os.path.join(source, item)
         tree_destination = os.path.join(destination, item)
         if os.path.isdir(tree_source):
@@ -172,8 +213,26 @@ def copytree(source, destination, symlinks=False, ignore=None):
             if not os.path.exists(tree_destination) or os.stat(tree_source).st_mtime - os.stat(tree_destination).st_mtime > 1:
                 shutil.copy2(tree_source, tree_destination)
                # print(tree_source + ' copied to ' + tree_destination)
+    return log_string
 
-def remove_files(source, destination, filelist):
+def help_text():
+    print("Help is for the weak.")
+    help_value = int(input("0=Integrity Overview, 1=Create_Filelist help,\n2=Validate help, 3=Emotional Support\n"))
+    if help_value == 0:
+        print(integrity_help)
+    elif help_value == 1:
+        help(create_filelist)
+    elif help_value == 2:
+        print(validate_help)
+        help(remove_files)
+        help(create_backups)
+        help(clean_backups)
+    elif help_value==3:
+        print('You can do it!')
+    else:
+        print('invalid input')
+
+def remove_files(source, destination, textfile):
     '''
     Removes unwanted files
     
@@ -186,57 +245,84 @@ def remove_files(source, destination, filelist):
         Path to folder to backup
     destination : str
         Path where backups are kept
-    filelist : li
-        list of strings of filenames
+    text file : .txt
+        text file of filenames seperated by new lines
 
     Returns
     -------
     Str
         Log of files removed
     '''
+
+    filelist = open(textfile,'r').read().split('\n')
+    
+    log_string = ''
     for file in os.listdir(source):
         if file not in filelist:
             if not os.path.exists(destination):
                 os.makedirs(destination)
             shutil.move(source + file, destination + file)
-            print (source + file + " moved to " + destination + file)
-    return
+            log_string += (source + file + " moved to " + destination + file + '\n')
 
+    return log_string
 def run_actions():
-    #Create Logs
-    print ('LOG FILE ')
-    print ('DATE: ' + str(date.today()))
-    print('')
-    print('')
-    print('FILES REMOVED: ')
-    print('')
-    remove_files(path, date_path, filelist)
-    print('')
-    print('')
-    print('BACKUPS CREATED: ')
-    print('')
-    create_backups(path,backup_destination,ignore_folder = ignore_folder)
-    print('')
-    print('')
-    print('BACKUPS REMOVED: ')
-    print('')
-    clean_backups(integrity_path + "backups/",10)
+    log_string = 'LOG FILE\nDATE: ' + str(date.today()) + '\n\nFILES REMOVED: \n'
+    filelist = open('filelist.txt')
+    log_string += remove_files(path, date_path,'filelist.txt') + '\n'
+    filelist.close()
+    log_string += ('\n\nBACKUPS CREATED: \n')
+    log_string += create_backups(path,backup_destination,ignore_folder = ignore_folder) + '\n'
+    log_string += '\n\nBACKUPS REMOVED: \n'
+    log_string += clean_backups(integrity_path + "backups/",10) + '\n'
+    return log_string
 
-def run():
+def auto_run():
+    print('Integrity:\nA Data Assurance Program\n   by Riley Mulvihill')
     while(1==1):
+        week_number = datetime.today().weekday()
+        if week_number < 5:
+            time = datetime.now()
+            print('Time: ' + str(time))
+            runtimes = (9,13,16)
+            if True:
+            #if time.hour == 8:
+                create_filelist(path)
+                print ('Filelist Created')
+            if time.hour in runtimes:
+                log_name = log_destination + str(date.today()) + '-' + str(time.hour) + '_' + str(time.minute) + '.txt'
+                log = open(log_name, 'w')
+                log.write(run_actions())
+                log.close()
+                print ('Log Created')
+            sleep (60*(60-datetime.now().minute))
+        else:
+            print('Sleeping over Weekend')
+            today = now = datetime.today()
+            today = datetime(today.year, today.month, today.day)
+            sleep (round((timedelta(days=7-now.weekday()) + today - now).total_seconds()))
+
+def manual_run():
+    print('Integrity:\nA Data Assurance Program\n   by Riley Mulvihill')
+    run = True
+    while(run):
         time = datetime.now()
-        print(time)
-        runtimes = (8,10,12,14,16)
-        if time.hour == 7:
+        value = int(input("\nEnter Value to Proceed\n0 = Create Filelist 1=Validate Data 2=Help 3=Quit\n"))
+        if value == 0:
             create_filelist(path)
-        if time.hour in runtimes:
-            run_actions()
-        sleep (60*(60-datetime.now().minute))
- 
+            print ('Filelist Created')
+        elif value == 1:
+            log_name = log_destination + str(date.today()) + '-' + str(time.hour) + '_' + str(time.minute) + '.txt'
+            log = open(log_name, 'w')
+            log.write(run_actions())
+            log.close()
+            print ('Log Created')
+        elif value == 2:
+            help_text()
+        elif value == 3:
+            run = False
+        else:
+            print('Invalid Entry')
+            
 #Run
 
-run()
-
-
-
-        
+manual_run()     
